61d122b921d1aa29602ee788f2b584d42f5a25ad
zerothy <zxuyagaming@gmail.com>
2024-02-27 00:29:18 +0700

    Add assign button and modals for money input

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,55 +2,36 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import Head from "next/head";
 import { useState } from "react";
+import { useEffect } from "react";
 import React from "react";
-import CurrencyInput, { CurrencyInputProps, CurrencyInputOnChangeValues } from 'react-currency-input-field';
-import CurrencyInputValue from 'react-currency-input-field';
 
 import { TbTriangleInvertedFilled } from "react-icons/tb";
+import { IoMdCheckmarkCircleOutline } from "react-icons/io";
+import { RiMoneyDollarCircleLine } from "react-icons/ri";
 
 const inter = Inter({ subsets: ["latin"] });
 
-const HomeHeader = () => {
+const HomeHeader: React.FC = () => {
   const [mouseDown, setMouseDown] = useState(false);
   const handleMouseDown = () => setMouseDown(true);
 
+  const [receiveMouseDown, setReceiveMouseDown] = useState(false);
+  const handleReceiveMouseDown = () => setReceiveMouseDown(true);
+  const handleReceiveMouseUp = () => setReceiveMouseDown(false);
+
+  const [spendMouseDown, setSpendMouseDown] = useState(false);
+  const handleSpendMouseDown = () => setSpendMouseDown(true);
+  const handleSpendMouseUp = () => setSpendMouseDown(false);
+
   const [showModal, setShowModal] = useState(false);
 
-  const [value, setValue] = useState<CurrencyInputProps>({
-    value: 0,
-    decimalsLimit: 2,
-    intlConfig: { locale: 'id-ID', currency: 'IDR' },
-    min: 0,
-    onChange: (event) => {
-      const newValue = event.target.value || 0;
-      const formattedValue = new Intl.NumberFormat('id-ID', {
-        style: 'currency',
-        currency: 'IDR',
-        minimumFractionDigits: 2,
-      }).format(Number(newValue));
-
-      setValue({
-        ...value,
-        value: newValue,
-      });
-    },
-  });
-
-  // TODO: FIX THIS FKIN CODE TO WORK 
-  // const handleChange = (event: CurrencyInputOnChangeValues<number>) => {
-  //   const newValue = event.value || 0;
-  //   const formattedValue = new Intl.NumberFormat('id-ID', {
-  //     style: 'currency',
-  //     currency: 'IDR',
-  //     minimumFractionDigits: 2,
-  //   }).format(newValue);
-
-  //   setValue({
-  //     value: newValue,
-  //     formattedValue: formattedValue,
-  //     error: newValue < 0,
-  //   });
-  // };
+  const [value, setValue] = useState<number>(NaN);
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.value === "") return setValue(NaN);
+    if (parseInt(e.target.value) < 0) return setValue(NaN);
+    if (parseInt(e.target.value) > 1000000000) return setValue(1000000000);
+    setValue(parseInt(e.target.value));
+  };
 
   const toggleModal = () => {
     setShowModal(!showModal)
@@ -77,15 +58,29 @@ const HomeHeader = () => {
       >
         <div 
           onClick={(e) => e.stopPropagation()}
-          className="w-96 h-96 bg-[#010048] rounded-xl border-4 border-[#ffc900] border-solid border-opacity-40 flex justify-center items-center"
-        >
-          <CurrencyInput
-            placeholder="Rp 0,00"
-            className="bg-slate-500 text-center rounded-xl px-3 pt-3 pb-8 outline-none font-monserrat font-extrabold text-2xl text-white no-spinners"
-            intlConfig={{ locale: 'id-ID', currency: 'IDR' }}
-            min={0}
-            decimalsLimit={2}
-          />
+          className="w-max h-max p-6 bg-gradient-to-br from-[#010048af] via-[#020078b2] to-[#0805adaf] rounded-xl border-4 border-[#ffc900] border-solid border-opacity-40 flex justify-center items-center"
+        > 
+          <div className="absolute mr-[16rem] mb-[6.6rem] text-white font-monserrat font-bold text-md">Amount</div>
+          <div className="flex flex-col items-center">
+            <input
+              placeholder="Rp 0,00"
+              className="bg-slate-500 rounded-xl px-3 pt-8 pb-3 outline-none font-monserrat font-extrabold text-2xl text-white no-spinners"
+              type="Number"
+              onChange={handleInputChange}
+              value={value}
+              max={1000000000}
+            />
+            <div className="flex justify-evenly p-4 w-full"> 
+              <div onMouseDown={handleReceiveMouseDown} onMouseUp={handleReceiveMouseUp} className={`bg-[#9092ff] text-gray-900 duration-150 cursor-pointer transition-all py-2 px-4 font-monserrat font-bold rounded-3xl ${receiveMouseDown ? 'scale-100' : 'hover:scale-105'}`}>
+                <IoMdCheckmarkCircleOutline className="inline-block mb-[0.2rem] mr-2" />
+                Receive
+              </div>
+              <div onMouseDown={handleSpendMouseDown} onMouseUp={handleSpendMouseUp} className={`bg-[#575899] text-slate-200 duration-150 cursor-pointer transition-all pl-4 py-2 pr-5 font-monserrat font-bold rounded-3xl ${spendMouseDown ? 'scale-100' : 'hover:scale-105'}`}>
+                <RiMoneyDollarCircleLine className="inline-block mb-[0.2rem] mr-2 text-xl" />
+                Spend
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
