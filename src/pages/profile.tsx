4d4b8e9cfd95715c768ea2167b5a800cd6975e7a
zerothy <zxuyagaming@gmail.com>
2024-03-06 23:18:32 +0700

    add some feature

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,7 +1,13 @@
-export default function Transactions() {
+import { CgProfile } from "react-icons/cg";
+
+
+export default function Profile() {
     return (
-        <div className="bg-[#03002e] h-dvh w-full md:w-[82%] overflow-hidden">
-            ehe
+        <div className="bg-[#03002e] h-dvh w-full md:w-[82%] overflow-hidden flex flex-col justify-center items-center">
+            <CgProfile className="text-white text-9xl" />
+            <div className="text-white text-6xl font-semibold">
+                Joe Mathew
+            </div>
         </div>
     );
 }
